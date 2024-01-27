955ad0d45b2ef45e6dc6205f5cf2c01ab99fc2c7
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-01-27 20:43:19 +0530

    Update FlippingDigit.cpp

--- Arrays/FlippingDigit.cpp
+++ Arrays/FlippingDigit.cpp
@@ -37,12 +37,5 @@ int main()
         }
     }
 
-    // for(int i = 0 ; i < r ; i++)
-    // {
-    //     for(int j = 0 ; j < c ; j++)
-    //     {
-    //         cout<<arr[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
+   
 }
