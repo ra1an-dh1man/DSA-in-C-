29c3d46d7c8fc656cb9d88c016cb2887f797c3ec
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-01-30 22:43:07 +0530

    Create BinarySearch.cpp

---
+++ Arrays/Binary Search/BinarySearch.cpp
@@ -0,0 +1,50 @@
+/*
+  condition for binary search is that , the array must be sorted.
+*/
+#include<iostream>
+using namespace std;
+
+int BinarySearch(int arr[], int n, int key)
+{
+    int start = 0;
+    int end = n - 1;
+
+    int mid = start + (end - start) / 2;
+
+    while(start <= end)
+    {
+        if(key == arr[mid])
+            return mid;
+        
+        else if(key > arr[mid])
+            start = mid + 1;
+        
+        else if(key < arr[mid])
+            end = mid - 1;
+
+        mid = start + (end - start) / 2;
+
+    }
+    return -1;
+};
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i = 0 ; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    int key;
+    cin>>key;
+
+    int res = BinarySearch(arr, n, key);
+
+    cout<<"Index of the key is -> " << res;
+
+return 0;
+}
