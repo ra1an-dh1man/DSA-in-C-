86af4cdf4aa25e46e761b3e6ca1b05792006925f
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-02-01 21:46:39 +0530

    Create Pivot.cpp

---
+++ Arrays/Binary Search/Pivot.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int getPivot(int arr[], int n)
+{
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
+
+    while(s < e)
+    {
+        if(arr[mid] >= arr[0])
+            s = mid + 1;
+        else
+            e = mid;
+
+        mid = s + (e - s) / 2;
+    }
+    return e;
+}
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<"Pivot element is at index -> " << getPivot(arr, n);
+
+}
