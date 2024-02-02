eae92736296aae122f422513c85e54bf5b1637c7
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-02-02 22:25:22 +0530

    Update BubbleSort.cpp

--- Arrays/BubbleSort.cpp
+++ Arrays/BubbleSort.cpp
@@ -14,11 +14,16 @@ int main()
 
     for(int i = 0 ; i < n - 1; i++)
     {
+        int swapped = false;
         for(int j = i + 1; j < n; j++)
         {
             if(arr[i] > arr[j])
                 swap(arr[i], arr[j]);
+            
+            swapped = true;
         }
+        if(swapped == false)
+            break;
     }
 
     for(int i = 0 ; i < 6 ; i++)
