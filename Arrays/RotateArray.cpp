5ddbeef36799b8e9b8aa657dcb48c91014a7ff9c
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-02-08 23:06:56 +0530

    Create RotateArray.cpp

---
+++ Arrays/RotateArray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin>>arr[i];
+    }
+
+    int rt;
+    cin>>rt;
+
+    int rtArray[n];
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        rtArray[(i + rt) % n] = arr[i];
+    }
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        cout<<rtArray[i]<<" ";
+    }
+}
