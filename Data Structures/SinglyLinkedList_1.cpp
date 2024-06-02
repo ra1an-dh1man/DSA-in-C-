ba40d7fcd2e1bb6f31aa3d5b8bd4b470e9596ede
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-06-02 13:52:39 +0530

    Create SinglyLinkedList_1.cpp
    insertion at head

---
+++ Data Structures/SinglyLinkedList_1.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public: 
+
+    int data;
+    Node* next;
+
+    Node(int data)
+    {
+        this -> data = data;
+        this -> next = nullptr;
+    }
+};  
+
+void insertAtHead(Node* &head, int data)
+{
+    Node* temp = new Node(data);
+    temp -> next = head;
+    head = temp;
+}
+
+void showList(Node* &head)
+{
+    Node* temp = head;
+
+    while(temp != nullptr)
+    {
+        cout<<temp -> data<<" ";
+        temp = temp ->next;
+    }
+    cout<<endl;
+}
+int main()
+{
+    Node* node = new Node(10);
+    Node* head = node;
+
+    showList(head);
+
+    insertAtHead(head, 65);
+    showList(head);
+
+    insertAtHead(head, 110);
+    showList(head);
+
+}
